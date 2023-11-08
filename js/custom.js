7bdc86ef3dcaa294538c2cb36d3b54be748f1fb1
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2023-11-08 18:07:59 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,23 @@ $(function(){
         arrows: false,
         autoplay: true,
         dots: true,
+        pauseOnHover: false,
         
     });
+
+    $('.Main_Product_Slide').slick({
+        slidesToShow: 3,
+        arrows: false,
+        dots: true
+    });
+
+    $('.MainProduct .arrows .left').on('click', function(){
+        $('.Main_Product_Slide').slick('slickPrev');
+    });
+
+    $('.MainProduct .arrows .right').on('click', function(){
+        $('.Main_Product_Slide').slick('slickNext');
+    });
+
 
 })
